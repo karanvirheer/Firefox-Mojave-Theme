bc3136aaf63c7224014fcc3337ba5be7849234d4
Karanvir Heer <karanvirheer77@gmail.com>
2025-03-16 16:14:40 -0400

    Finished -> Confirming choices of color -> WIP

--- background.js
+++ background.js
@@ -38,11 +38,10 @@ const themes = {
       // Icon Colour
       //===============
 
-      // color of buttons when you hover over
-      button_background_hover: "#efedeb",
+      // colour of taskbar items when you hover over
+      button_background_hover: "#495261",
 
       // taskbar icons
-      icons: "#1a2334",
       icons: "#efedeb",
 
       // bookmark star in searchbar
@@ -58,7 +57,7 @@ const themes = {
       // Tab Colours
       //===============
 
-      // color of text of tab names and bookmarks (ig?)
+      // colour of text of tab names and bookmarks (ig?)
       tab_background_text: "#efedeb",
 
       tab_loading: "#efedeb",
@@ -68,39 +67,67 @@ const themes = {
       // Pop-Up Colours
       //===============
 
-      // hover tab popup color
-      // popup: "#efedeb",
+      // hover tab popup colour
       popup: "#1a2334",
 
       // selected URL in searchbar
       popup_highlight: "#1a2334",
 
-      // text color of items highlighted inside popups
-      // searchbar recommended links text color
+      // text colour of items highlighted inside popups
+      // searchbar recommended links text colour
       popup_highlight_text: "#efedeb",
 
-      // text color of when you hover over a tab
+      // text colour of when you hover over a tab
       popup_text: "#efedeb",
 
-      // bookmarked item > the color of that menu border
+      // bookmarked item > the colour of that menu border
       popup_border: "#efedeb",
 
       //===============
       // Searchbar Colours
       //===============
 
+      // toolbar colour
+      // commenting this out reveals the img transparency
+      toolbar: "#1a2334",
+
+      // colour of the text in the toolbar (not typing)
+      toolbar_field_text: "#efedeb",
+
+      // colour of the text in the toolbar (when typing)
+      toolbar_field_text_focus: "#efedeb",
+
+      // searchbar colour
       toolbar_field: "#4d586e",
 
-      // toolbar color
-      // commenting this out reveals the img transparency
-      // toolbar: "#4d586e",
-      // toolbar: "#1a2334",
+      // searchbar border colour (when not typing)
+      toolbar_field_border: "#495261",
+
+      // searchbar border colour (when typing)
+      toolbar_field_border_focus: "#efedeb",
+
+      // searchbar colour (when typing)
+      toolbar_field_focus: "#4d586e",
+
+      // ============================
+      // !!!!!!!!! TEST OUT !!!!!!!!!!!!
+      // ============================
+
+      // searchbar highlight colour
+      toolbar_field_highlight: "#efedeb",
+
+      // searchbar highlight text colour
+      toolbar_field_highlight_text: "#1a2334",
+
+      // ============================
+      // !!!!!!!!! TEST OUT !!!!!!!!!!!!
+      // ============================
 
       //===============
       // Sidebar Border Colours
       //===============
 
-      // bookmarks manager // history sidebar color
+      // bookmarks manager // history sidebar colour
       sidebar: "#1a2334",
       sidebar_border: "#efedeb",
 
@@ -136,11 +163,12 @@ function checkTime() {
   let date = new Date();
   let hours = date.getHours();
   // Will set the sun theme between 8am and 8pm.
-  if (hours > 8 && hours < 19) {
-    setTheme("day");
-  } else {
-    setTheme("night");
-  }
+  setTheme("night");
+  // if (hours > 8 && hours < 19) {
+  //   setTheme("day");
+  // } else {
+  //   setTheme("night");
+  // }
 }
 
 // On start up, check the time to see what theme to show.
